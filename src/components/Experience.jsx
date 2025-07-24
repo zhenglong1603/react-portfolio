ff02d5733ce67efa665a49ce3d885a34d433ee55
zhenglong1603 <139528731+zhenglong1603@users.noreply.github.com>
2025-07-25 01:20:59 +0800

    Fix formatting of technologies and font colour for light mode

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,44 +1,60 @@
 import { EXPERIENCES } from "../constants";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 const Experience = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-        <motion.h1 
-        whileInView={{opacity: 1, y: 0 }}
-        initial={{opacity: 0, y: -100}}
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 0.5 }}
-        className="my-20 text-center text-4xl">Experience</motion.h1>
-        <div>
-            {EXPERIENCES.map((experience, index) => (
-                <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-                    <motion.div 
-                    whileInView={{ opacity: 1, x: 0 }}
-                    initial={{ opacity: 0, x: -100 }}
-                    transition={{duration: 1 }}
-                    className="w-full lg:w-1/4">
-                        <p className="mb-2 text-sm text_neutral-400">{experience.year}</p>
-                    </motion.div>
-                    <motion.div
-                    whileInView={{ opacity: 1, x: 0 }}
-                    initial={{ opacity: 0, x: 100 }}
-                    transition={{duration: 1 }}
-                    className="w-full max-w-xl lg:w-3/4">
-                        <h6 className=" mb-2 font-semibold">
-                            {experience.role} - <span className="text-sm text-purple-100">
-                                {experience.company}
-                            </span>
-                        </h6>
-                        <p className="mb-4 text-neutral-400 whitespace-pre-line">{experience.description}</p>
-                        {experience.technologies.map((tech, index) => (
-                            <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-500">{tech}</span>
-                        ))}
-                    </motion.div>
-                </div>
-            ))}
-        </div>
+        className="my-20 text-center text-4xl"
+      >
+        Experience
+      </motion.h1>
+      <div>
+        {EXPERIENCES.map((experience, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="w-full lg:w-1/4"
+            >
+              <p className="mb-2 text-sm text_neutral-400">{experience.year}</p>
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className="w-full max-w-xl lg:w-3/4"
+            >
+              <h6 className=" mb-2 font-semibold">
+                {experience.role} -{" "}
+                <span className="text-sm dark:text-purple-300">
+                  {experience.company}
+                </span>
+              </h6>
+              <p className="mb-4 whitespace-pre-line">
+                {experience.description}
+              </p>
+              {/* Tech tags container */}
+              <div className="flex flex-wrap gap-2 mt-4">
+                {experience.technologies.map((tech, index) => (
+                  <span
+                    key={index}
+                    className="rounded bg-purple-600 text-white dark:bg-neutral-900 dark:text-purple-500 px-2 py-1 text-sm font-medium"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </motion.div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,54 +1,73 @@
-import { PROJECTS } from "../constants"
-import {motion} from "framer-motion"
+import { PROJECTS } from "../constants";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-        <motion.h1 
+      <motion.h1
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
-        transition={{duration: 0.5 }}className="my-20 text-center text-4xl">Projects</motion.h1>
-        <div>
-            {PROJECTS.map((project, index) => (
-                <div key={index} className="mb-8 flex flex-wrap lg:justify-center gap-x-8">
-                    <motion.div 
-                    whileInView={{ opacity: 1, x: 0 }}
-                    initial={{ opacity: 0, x: -100 }}
-                    transition={{duration: 1 }}className="w-full lg:w-1/4">
-                    <img 
-                        src={project.image} 
-                        width={260}
-                        height={260}
-                        alt={project.title}
-                        className="mb-6 rounded" />
-                    </motion.div>
-                    <motion.div 
-                    whileInView={{ opacity: 1, x: 0 }}
-                    initial={{ opacity: 0, x: 100 }}
-                    transition={{duration: 1 }}
-                    className="w-full max-w-xl lg:w-3/4">
-                        <h6 className="mb-2 font-semibold">{project.title}</h6>
-                        <p className="mb-4 text-neutral-400 whitespace-pre-line">{project.description}</p>
-                        {project.technologies.map((tech, index) => (
-                            <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-500">{tech}</span>
-                        ))}
-                        <br />
-                        {project.link && (
-                         <a
-                         href={project.link}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="inline-block mt-2 rounded bg-purple-600 px-4 py-2 text-sm text-white transition hover:bg-purple-700"
-                            >
-                            View Project
-                        </a>
-                    )}
-                    </motion.div>
-                </div>
-            ))}
-        </div>
-    </div>
-  )
-}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Projects
+      </motion.h1>
+      <div>
+        {PROJECTS.map((project, index) => (
+          <div
+            key={index}
+            className="mb-8 flex flex-wrap lg:justify-center gap-x-8"
+          >
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="w-full lg:w-1/4"
+            >
+              <img
+                src={project.image}
+                width={260}
+                height={260}
+                alt={project.title}
+                className="mb-6 rounded"
+              />
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className="w-full max-w-xl lg:w-3/4"
+            >
+              <h6 className="mb-2 font-semibold">{project.title}</h6>
+              <p className="mb-4 whitespace-pre-line">{project.description}</p>
 
-export default Projects
+              {/* Tech tags container */}
+              <div className="flex flex-wrap gap-2 mt-4">
+                {project.technologies.map((tech, index) => (
+                  <span
+                    key={index}
+                    className="rounded bg-purple-600 text-white dark:bg-neutral-900 dark:text-purple-500 px-2 py-1 text-sm font-medium"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              {project.link && (
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-block mt-2 rounded bg-purple-600 px-4 py-2 text-sm font-medium text-white transition hover:bg-purple-700"
+                >
+                  View Project
+                </a>
+              )}
+            </motion.div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
