9ee2b38b5063d045c5adb9624250de942b574e0e
zhenglong1603 <139528731+zhenglong1603@users.noreply.github.com>
2025-07-25 01:18:51 +0800

    Add themeProvider to App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,18 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
-
 const App = () => {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+    <div className="overflow-x-hidden text-black dark:text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 transition-colors duration-300">
       <div className="fixed top-0 -z-10 h-full w-full">
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        <div 
+          className="absolute top-0 z-[-2] h-screen w-screen 
+          bg-white dark:bg-neutral-950
+          bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.15),rgba(255,255,255,0))]
+          dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
+        ></div>
       </div>
-      
+
       <div className="container mx-auto px-8">
         <Navbar />
         <Hero />
@@ -24,7 +28,7 @@ const App = () => {
         <Contact />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
